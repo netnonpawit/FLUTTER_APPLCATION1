e3d0d331d5d4461262831c6ad69a265dfe68066f
net <netburiram1@gmail.com>
2022-08-04 17:11:24 +0700

    first commit

--- lib/states/testgps/screens/gps_model.dart
+++ lib/states/testgps/screens/gps_model.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/widgets.dart';
+
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class locator {
   String locatorName;
   String address;
-  String descripstion;
+  String description;
   String thumNail;
   LatLng locationCoords;
   //Image image;
@@ -12,7 +12,7 @@ class locator {
   locator(
       {required this.locatorName,
       required this.address,
-      required this.descripstion,
+      required this.description,
       required this.thumNail,
       required this.locationCoords,
       //required this.image});
@@ -22,7 +22,7 @@ final List<locator> lacation = [
   locator(
       locatorName: 'ChangArena',
       address: '18 W 29th St',
-      descripstion:
+      description:
           'ChangArena Football Stadium',
       locationCoords: LatLng(14.9658746, 103.0921753),
       thumNail: 'https://lh5.googleusercontent.com/p/AF1QipNNzoa4RVMeOisc0vQ5m3Z7aKet5353lu0Aah0a=w90-h90-n-k-no'
--- lib/states/testgps/screens/search.dart
+++ lib/states/testgps/screens/search.dart
@@ -18,7 +18,9 @@ class _SearchState extends State<Search> {
   List<Marker> allMarkers = [];
    late PageController _pageController;
    late int prevPage;
-  
+   late int _currentPage;
+
+ 
   @override
   void initState(){
      super.initState();
@@ -28,6 +30,12 @@ class _SearchState extends State<Search> {
      });
      _pageController = PageController(initialPage: 1, viewportFraction: 0.8)
       ..addListener(_onScroll);
+       _currentPage = 0;
+    _pageController.addListener(() {
+      setState(() {
+        _currentPage = _pageController.page!.toInt();
+      });
+    });
   }
    void _onScroll() {
     if (_pageController.page as int  != prevPage) {
@@ -64,6 +72,7 @@ class _SearchState extends State<Search> {
 
     userLocation = await Geolocator.getCurrentPosition();
     return userLocation;
+    
   }
 
   Marker ChangArena = Marker(
@@ -82,7 +91,93 @@ class _SearchState extends State<Search> {
       infoWindow: InfoWindow(title: 'HomeProBR'),
       icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed));
       
-     
+     _locatorList (index){
+      return AnimatedBuilder(
+      animation: _pageController,
+       builder: (BuildContext context, Widget? child) {
+        double value = 1;
+        if (_pageController.position.haveDimensions) {
+          value = _pageController.page ??  - index;
+          value = (1 - (value.abs() * 0.3) + 0.06).clamp(0.0, 1.0);
+        }
+        return Center(
+          child: SizedBox(
+            height: Curves.easeInOut.transform(value) * 125.0,
+            width: Curves.easeInOut.transform(value) * 350.0,
+            child: widget,
+        ));
+      },
+      child: InkWell(
+          onTap: () {
+            // moveCamera();
+          },
+          child: Stack(children: [
+            Center(
+                child: Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                      vertical: 20.0,
+                    ),
+                    height: 125.0,
+                    width: 275.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black54,
+                            offset: Offset(0.0, 4.0),
+                            blurRadius: 10.0,
+                          ),
+                        ]),
+                    child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.white),
+                        child: Row(children: [
+                          Container(
+                              height: 90.0,
+                              width: 90.0,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(10.0),
+                                      topLeft: Radius.circular(10.0)),
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                          lacation[index].thumNail),
+                                      fit: BoxFit.cover))),
+                          SizedBox(width: 5.0),
+                          Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  lacation[index].locatorName,
+                                  style: TextStyle(
+                                      fontSize: 12.5,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  lacation[index].address,
+                                  style: TextStyle(
+                                      fontSize: 12.0,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                Container(
+                                  width: 170.0,
+                                  child: Text(
+                                    lacation[index].description,
+                                    style: TextStyle(
+                                        fontSize: 11.0,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                )
+                              ])
+                        ]))))
+          ])),
+    );
+  }
+ 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -123,7 +218,7 @@ class _SearchState extends State<Search> {
        Positioned(child: Container(height: 200.0,
        width: MediaQuery.of(context).size.width,
        child:PageView.builder(  controller: _pageController,
-                  itemCount: locator.length,
+                  itemCount: lacation.length,
                   itemBuilder: (BuildContext context, int index) {
                     return _locatorList(index);} ,))
       )],
@@ -151,7 +246,7 @@ class _SearchState extends State<Search> {
 
   moveCamera() {
     mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-        target: locator[_pageController.page.toInt()].locationCoords,
+        target: locator[_currentPage].locationCoords,
         zoom: 14.0,
         bearing: 45.0,
         tilt: 45.0)));
